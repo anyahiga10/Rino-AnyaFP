30b2763169bec658775c8e7978dbf563de946451
anyahiga10 <ahiga10@uw.edu>
2023-05-03 11:27:45 -0700

    added all data frames

---
+++ data_wrangling.R
@@ -0,0 +1,7 @@
+account_ownership_df <- read.csv("Account ownership at a financial institution or with a mobile-money-service provider (% of population ages 15+).csv")
+made_recieved_digital_payments_df <- read.csv("Made or received digital payments in the past year (% age 15+).csv")
+umpi_access_account_df <- read.csv("Used a mobile phone or the internet to access an account (% with an account, age 15+).csv")
+umpi_buy_something_online_df <- read.csv("Used a mobile phone or the internet to buy something online in the past year(% age 15+).csv")
+umpi_pay_bills_df <- read.csv("Used a mobile phone or the internet to pay bills in the past year (% age 15+).csv")
+
+adolescent_df <- read.csv("AIU Adolescent Dataset.csv")
